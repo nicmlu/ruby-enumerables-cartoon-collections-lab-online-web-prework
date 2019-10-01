0fb0cdd9083873d127575d7a3214732321dac3cf
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 18:32:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,15 @@ def long_planeteer_calls(array)
   end
 end
 
+# def find_the_cheese(array)
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#     array.find do |cheese|
+#       cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+#     end
+# end
+
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |cheese|
-      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-    end
+  array.include?("cheddar", "gouda", "camembert")
 end
