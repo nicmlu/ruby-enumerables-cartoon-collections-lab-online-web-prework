409c654f2ce86d78f37962fe34a6c5f273db46a4
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 17:22:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.include?(element.length >= 4)
+  array.include?(array.length >= 4)
 end
 
 def find_the_cheese# code an argument here
