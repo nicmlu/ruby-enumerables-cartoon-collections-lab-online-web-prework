3b106046815e2b69336914da0665cb13ebb6bff9
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 17:25:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(array, element)
+def long_planeteer_calls(array)
   i = 0
   array.include?(element.length >= 4)
   i += 1
