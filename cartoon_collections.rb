f5edbbc0184decdd4d7852e9a624c8f69ef24f81
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 18:26:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-    
+    array.find do |cheese|
+      cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
 end
