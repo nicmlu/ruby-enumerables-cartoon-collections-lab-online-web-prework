02616207f52bdbb926eacabd32cb9c615a7e69aa
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-01 17:38:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,7 @@ def long_planeteer_calls(array)
   i = 0
   array.include?(array[i].length >= 4)
   i += 1
-  true
-end
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
